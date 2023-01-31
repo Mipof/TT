df1596cee32c727e1db418c2df2d713a6f42fb45
Mipof <miklosramon@gmail.com>
2023-01-31 11:46:58 -0600

    LookToMovement

--- Assets/Scripts/LookToMovement.cs
+++ Assets/Scripts/LookToMovement.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class LookToMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField][Range(0,20)][Min(1)] int _rotationSpeed = 20;
+    private float _maxRadiansDelta = 1f;
+    private Vector3 _previousPosition;
+    private Transform _transform;
+
+    private void Awake()
     {
-        
+        _transform = transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector3 currentDirection = _transform.position - _previousPosition;
+        Vector3 targetDirection = Vector3.RotateTowards(_transform.forward, currentDirection, _maxRadiansDelta,
+            Time.deltaTime);
+        _transform.rotation = Quaternion.RotateTowards(_transform.rotation, Quaternion.LookRotation(targetDirection),
+            Time.deltaTime * _rotationSpeed);
+        _previousPosition = _transform.position;
     }
 }
