6ba7c20f8d0a378b337b52c42b64424c56d2d531
judkauwe <santilink97@hotmail.com>
2023-02-03 13:26:20 -0600

    Frog tongue

---
+++ Assets/Tongue_attack.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tongue_attack : MonoBehaviour
+{
+
+    LineRenderer line;
+    Vector3 originalpos = new Vector3(540.6785f, -6.339138f, 1.0f);
+    private void Awake()
+    {
+      
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void tongue (Vector3 position)
+    {
+        line.SetPosition(0, position);
+    }
+
+    public void tongueoriginalpos ()
+    {
+        line.SetPosition(0, originalpos);
+    }
+}
